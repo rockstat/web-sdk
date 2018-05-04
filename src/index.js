363fb0eb9f076abb735d5c045fa5468232bfd785
Dmitry Rodin <madiedinro@gmail.com>
2018-05-05 01:56:38 +0300

    work on multi-configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './polyfill';
-import {documentReady} from './functions/domEvents';
-import {win} from './Browser';
+import {
+  documentReady
+} from './functions/domEvents';
+import {
+  win
+} from './Browser';
 
 // Main
 import Alcolytics from './Alcolytics';
@@ -8,23 +12,31 @@ import Alcolytics from './Alcolytics';
 const holder = win['alco'];
 
 if (holder) {
-  const alcolytics = new Alcolytics();
-  alcolytics.configure({
-    server: holder.server,
-    snippet: holder.snippet
-  });
+  const buildTracker = function (stub) {
 
-// Attaching method to page
-  const doCall = function (args) {
-    args = args.slice(0);
-    const method = args.shift();
-    return alcolytics[method]
-      ? alcolytics[method].apply(alcolytics, args)
-      : new Error('Method not supported');
-  };
-  holder.queue.map(doCall);
-  holder.doCall = doCall;
-  holder.queue = [];
+    const tracker = new Alcolytics();
+    tracker.configure({
+      server: holder.server,
+      snippet: holder.snippet
+    });
+
+    // Attaching method to page
+    const doCall = function (args) {
+      args = args.slice(0);
+      const method = args.shift();
+      return alcolytics[method] ?
+        alcolytics[method].apply(alcolytics, args) :
+        new Error('Method not supported');
+    };
+
+
+    holder.queue.map(doCall);
+    holder.doCall = doCall;
+    holder.queue = [];
+
+  }
+
+
 
   documentReady(() => {
     alcolytics.initialize();
