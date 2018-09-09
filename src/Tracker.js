3670c81d0aab02636ccde198f1b635b1b8f03f14
Dmitry Rodin <madiedinro@gmail.com>
2018-09-10 01:25:42 +0300

    Bump version: 3.14.3 → 3.14.4

--- src/Tracker.js
+++ src/Tracker.js
@@ -54,7 +54,7 @@ import {
 } from './Browser';
 
 
-const LIBRARY = 'web-sdk-3.14.3';
+const LIBRARY = 'web-sdk-3.14.4';
 const LIBVER = 0;
 const noop = () => { };
 const log = createLogger('RST');
