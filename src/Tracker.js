6b1d66d8426221a96ccc986f28e449c0cf5a5fd4
Dmitry Rodin <madiedinro@gmail.com>
2020-03-14 22:47:33 +0300

    Bump version: 4.0.1 → 4.1.0

--- src/Tracker.js
+++ src/Tracker.js
@@ -55,7 +55,7 @@ import { packSemVer } from './functions/packSemVer';
 
 
 const LIBRARY = 'web-sdk';
-const LIBVER = packSemVer('4.0.1');
+const LIBVER = packSemVer('4.1.0');
 
 const noop = () => { };
 const asObject = (options) => {
