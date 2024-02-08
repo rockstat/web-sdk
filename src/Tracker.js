5800da15b387477d863fa253f5220842e96c788a
Dmitry Rodin <madiedinro@gmail.com>
2024-02-08 21:23:35 +0300

    Bump version: 4.2.10 → 4.2.11

--- src/Tracker.js
+++ src/Tracker.js
@@ -55,7 +55,7 @@ import { packSemVer } from './functions/packSemVer';
 
 
 const LIBRARY = 'web-sdk';
-const LIBVER = packSemVer('4.2.10');
+const LIBVER = packSemVer('4.2.11');
 
 const noop = () => { };
 const asObject = (options) => {
