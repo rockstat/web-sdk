df2f9fa544fa30d045506e1fc6297f4b37ecf571
Dmitry Rodin <madiedinro@gmail.com>
2024-08-06 05:58:33 +0300

    new digit

--- src/Transport.js
+++ src/Transport.js
@@ -28,7 +28,8 @@ import {
 import objectAssign from './functions/objectAssing';
 import createLogger from './functions/createLogger';
 import nextTick from './functions/nextTick'
-import simpleHash from './functions/simpleHash';
+// import simpleHash from './functions/simpleHash';
+import { cyrb53 } from './functions/cyrb53';
 // import { isObject } from './functions/type';
 
 const HTTPS = 'https';
@@ -36,6 +37,7 @@ const HTTPS = 'https';
 const log = createLogger('RST/Transport');
 const noop = () => { };
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 /**
  * Transport class containing general connecting methods
@@ -89,28 +91,31 @@ Transport.prototype.makeURL = function (path, data = {}, proto = HTTPS) {
   return `${proto}://${this.server}${this.pathPrefix}${path}?${query}`;
 }
 
-/**
- *
- * @param url {string}
- */
-Transport.prototype.sendIMG = function (url) {
 
-  const p = new Promise((resolve, reject) => {
-    const img = win.Image ? (new Image(1, 1)) : doc.createElement('img');
-    // img.onload = () => {
-    //   resolve();
-    // };
-    // img.onerror = (err_msg) => {
-    //   reject(err_msg);
-    // }
-    // img.onload = () => console.log('ok');
-    // img.onerror = (err_msg) => console.warn(err_msg);
-    img.src = url;
-    nextTick(() => resolve())
+/*
+if (false && hasFetchSupport) {
+  return fetch(postUrl, {
+    method: 'POST',
+    body: data,
+    keepalive: true
+  }).then((response) => {
+    return response.text()
+      .then((responseText) => {
+        console.log('resp text', responseText, response.status, response.status === 200);
+        if (response.status === 200) {
+          try {
+            return Promise.resolve(JSON.parse(responseText));
+          } catch (e) {
+            log.error(e)
+            return Promise.reject(e);
+          }
+        }
+      });
+  }).catch((error) => {
+    log.warn('Fetch failed', error);
   });
-  return p;
-};
-
+}
+*/
 
 /**
  *
@@ -124,7 +129,7 @@ Transport.prototype.sendIMG = function (url) {
  */
 Transport.prototype.send = function (msg, options = {}) {
   const data = JSON.stringify(msg);
-  const dig = simpleHash(data);
+  const dig = cyrb53(data);
   const isRequest = !!options[EVENT_OPTION_REQUEST];
   // !options[EVENT_OPTION_TERMINATOR] || !!options[EVENT_OPTION_OUTBOUND] || 
   const useTransportImg = !!options[EVENT_OPTION_TRANSPORT_IMG];
@@ -132,54 +137,132 @@ Transport.prototype.send = function (msg, options = {}) {
   const postPath = `/${this.urlMark}/${_service}.json`;
   const imgPath = `/${this.urlMark}/${_service}.gif`;
 
-  if (!useTransportImg) {
+  const postUrlBeacon = this.makeURL(postPath, { "dig": dig, "td_trans": "b"});
+  const postUrlFetch = this.makeURL(postPath, { "dig": dig, "td_trans": "f" });
+  // Use extra transport - img
+  // Send only part when using gif
+  return new Promise((resolve, reject) => {
+    if (useTransportImg) {
+      try {
+        const smallMsg = (msg.service === SERVICE_TRACK)
+          ? this.options.msgCropper(msg) : msg;
+        log.info(`Trying with img transport`, smallMsg);
+
+        const imgUrl = this.makeURL(imgPath, objectAssign(smallMsg, this.creds));
+
+        const img = win.Image ? (new Image(1, 1)) : doc.createElement('img');
+        img.src = imgUrl;
+        return nextTick(() => resolve());
+      } catch (e) {
+        return nextTick(() => reject(e));
+      }
+    }
+
     try {
-      const postUrl = this.makeURL(postPath, { "dig": dig });
+
+      let beaconResult = false;
+
+      if (this.options.allowSendBeacon && hasBeaconSupport) {
+        log.info('Sending using beacon');
+        beaconResult = nav.sendBeacon(postUrlBeacon, data);
+      }
+
+      if (beaconResult) {
+        return nextTick(() => resolve());
+      }
+
       if (hasFetchSupport) {
-        return fetch(postUrl, {
+        log.info('Sending using fetch');
+        fetch(postUrlFetch, {
           method: 'POST',
           body: data,
           keepalive: true
         }).then((response) => {
-          return response.text()
-            .then((responseText) => {
-              console.log('resp text', responseText, response.status, response.status === 200);
-              if (response.status === 200) {
-                try {
-                  return Promise.resolve(JSON.parse(responseText));
-                } catch (e) {
-                  log.error(e)
-                  return Promise.reject(e);
-                }
+          return response.then((responseText) => {
+            if (response.status === 200) {
+              try {
+                return JSON.parse(responseText);
+              } catch (e) {
+                return Promise.reject(e)
               }
-            });
-        }).catch((error) => {
-          log.warn('Fetch failed', error);
+            }
+          });
+        }).then((data) => {
+          return resolve(data)
+        }).catch((e) => {
+          return reject(e)
         });
       }
-      if (this.options.allowSendBeacon && hasBeaconSupport && !isRequest) {
-        log.info('sending using beacon');
-        nav.sendBeacon(postUrl, data);
-        return Promise.resolve();
-      }
-      if (isRequest) {
-        return Promise.reject('Requested request transport, but method unavailable');
-      }
-    } catch (error) {
-      log.warn('Beacon/XHR failed', error);
+    } catch (e) {
+      log.warn('Fetch err', e);
     }
-  }
+    return reject(e)
+  });
 
-  // Use extra transport - img
-  // Send only part when using gif
-  const smallMsg = (msg.service === SERVICE_TRACK)
-    ? this.options.msgCropper(msg) : msg;
-  log.info(`Trying to send using img`, smallMsg);
 
-  try {
-    return this.sendIMG(this.makeURL(imgPath, objectAssign(smallMsg, this.creds)));
-  } catch (e) {
-    log.warn('Error during sending data using image', e);
-    return Promise.reject(e);
-  }
+  // => response.text()
+  // .then((responseText) => {
+  //   console.log('resp text', responseText, response.status, response.status === 200);
+  //   if (response.status === 200) {
+  //     try {
+  //       resolve(JSON.parse(responseText));
+  //     } catch (e) {
+  //       log.error(e)
+  //       return reject(e);
+  //     }
+  //   }
+  // }
+
+
+
+  // return new Promise((resolve, reject) => {
+
+  //   const doSendBeacon = (attempt) => {
+  //     log.warn(`Sending beacon. Attempt ${attempt}`);
+  //     let res = nav.sendBeacon(postUrl, data);
+  //     if (res === true) {
+  //       return resolve();
+  //     }
+  //     if (attempt > 0) {
+  //       setTimeout(doSendBeacon(attempt - 1), 200);
+  //     } else {
+  //       nextTick(() => doSendImg());
+  //     }
+  //   }
+
+  //   const doFetch = () => {
+
+
+
+  //   }
+
+  //   // if (false && hasFetchSupport) {
+  //   // }
+
+  //   if (isRequest) {
+  //     return doFetch();
+  //     // return reject('Requested request transport, but method unavailable');
+  //   }
+
+  //   if (useTransportImg) {
+  //     return doSendImg();
+  //   }
+  // } catch (error) {
+  //   log.warn('Beacon/XHR failed', error);
+  //   return reject(error);
+  // }
+
+  // try {
+  //   return this.sendIMG(this.makeURL(imgPath, objectAssign(smallMsg, this.creds)));
+  // } catch (e) {
+  //   log.warn('Error during sending data using image', e);
+  //   return Promise.reject(e);
+  // }
+
+  // });
+
+
+
+
+
 };
